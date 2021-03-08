ca0154e0c5694f39e0793b09b9dfc493d4b1f5f1
Sabariswaran R <sabaris.rmv@gmail.com>
2021-03-08 07:14:01 +0530

    added user played task list

--- controllers/taskPlay.js
+++ controllers/taskPlay.js
@@ -119,3 +119,135 @@ exports.taskPlayCreate = function (req, res) {
         }
     })
 };
+
+exports.getUserPalyedTask = function (req, res) {
+    console.log("user info")
+    var arr = [];
+    arr = [
+        {
+            $lookup:{
+                from: "tasks",
+                localField: "task",
+                foreignField: "_id",
+                as: "task",
+            }
+        },
+        {
+            $project:{
+                user:1,
+                contestants:1,
+                createdAt:1,
+                earnPoint:1,
+                status:1,
+                userRanking:1,
+                task: { $arrayElemAt: ["$task", 0] }
+            }
+        },
+        {
+            "$group": {
+                _id: { user: "$user.userName" },
+                user: { "$push": "$user" },
+                earnPoint: { $sum: "$earnPoint" },
+                tasks: { "$push": "$task" }
+            },
+        },
+        {
+            "$project": {
+                "user": { $arrayElemAt: ["$user", 0] },
+                "_id": 0,
+                "earnPoint": 1,
+                "tasks": 1
+            }
+        },
+        {
+            "$sort": {
+                "earnPoint": -1
+            }
+        },
+        { 
+            $group: {
+                _id: "",
+                rankings: {
+                    $push: "$$ROOT"
+                }
+            }
+        },
+        { 
+            $unwind: {
+                path: "$rankings",
+                includeArrayIndex: "rank"
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "userId": "$rankings.user._id",
+                "tasks": "$rankings.tasks",
+                "user": "$rankings.user",
+                "earnPoint": "$rankings.earnPoint",
+                "rank": 1,
+                // "sameRankCount": { $size: "$rankings.participants" }
+            }
+        },
+        {
+            $match: { "user._id": ObjectId(req.id)  }
+        },
+      {
+            $lookup: {
+                from: "contestants",
+                localField: "tasks.winningContestants",
+                foreignField: "_id",
+                as: "winningContestants",
+            },
+        },
+        { 
+            $unwind: {
+                path: "$tasks",
+            }
+        },
+        {
+            $addFields: {
+                "week": "$tasks.week",
+            } 
+        },
+        {
+            "$sort": {
+                "week": 1
+            }
+        }
+    ]
+
+    var taskPlayDB = getModelByShow(req.db, "taskplay", taskPlayModel);
+
+    var aggregate = taskPlayDB.aggregate(arr);
+
+    var options = {
+        page: req.query.page || 1,
+        limit: parseInt(req.query.limit) || 200,
+    };
+
+    taskPlayDB.aggregatePaginate(aggregate, options, function (
+        err,
+        listdata,
+        pageCount,
+        count
+    ) {
+        if (err) {
+            console.log(err)
+            logger.error(`Error while list the tasks.`);
+            res.status(400).json({
+                apiName: "User Played Tasks",
+                success: false,
+                message: "Some Error Occured",
+            });
+        } else {
+            logger.info(`Ranking has been listed successfully.`);
+            res.json({
+                apiName: "User Played Tasks",
+                success: true,
+                message: "Task has been listed successfully",
+                taskList: listdata,
+            });
+        }
+    });
+}
